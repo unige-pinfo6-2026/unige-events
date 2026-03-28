9b8ae72ed7236c314d51016367c01277192ea64f
Agon <agon-kolgeci@hotmail.com>
2026-03-28 02:38:04 +0100

    Fix vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     plugins: [react()],
     server: {
