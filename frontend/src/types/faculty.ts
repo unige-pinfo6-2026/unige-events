cb72587b7b29d44a1aa046e2f677eae07ec37aeb
Agon <agon-kolgeci@hotmail.com>
2026-04-14 16:09:14 +0200

    Hotfix faculty logo 2023
    Wrong colors
    Add Social Sciences

--- frontend/src/types/faculty.ts
+++ frontend/src/types/faculty.ts
@@ -1,22 +1,23 @@
-import { Economy } from "@/assets/faculty/Economy"
-import { Law } from "@/assets/faculty/Law"
-import { Letters } from "@/assets/faculty/Letters"
-import { Medicine } from "@/assets/faculty/Medicine"
-import { Psychology } from "@/assets/faculty/Psychology"
-import { Sciences } from "@/assets/faculty/Sciences"
-import { Theology } from "@/assets/faculty/Theology"
-import { Translation } from "@/assets/faculty/Translation"
+import EconomyLogo from "@/assets/faculty/EconomyLogo"
+import LawLogo from "@/assets/faculty/LawLogo"
+import LettersLogo from "@/assets/faculty/LettersLogo"
+import MedicineLogo from "@/assets/faculty/MedicineLogo"
+import PsychologyLogo from "@/assets/faculty/PsychologyLogo"
+import SciencesLogo from "@/assets/faculty/SciencesLogo"
+import SocialSciencesLogo from "@/assets/faculty/SocialSciencesLogo"
+import TheologyLogo from "@/assets/faculty/TheologyLogo"
+import TranslationLogo from "@/assets/faculty/TranslationLogo"
 
 export const FACULTIES = {
-  SCIENCES: { name: 'Faculté des Sciences', abbr: 'Sciences', logo: Sciences, color: '#318063' },
-  MEDICINE: { name: 'Faculté de Médecine', abbr: 'Médecine', logo: Medicine, color: '#9a0050' },
-  LETTERS: { name: 'Faculté des Lettres', abbr: 'Lettres', logo: Letters, color: '#046fcb' },
-  SOCIAL_SCIENCES: { name: 'Faculté des Sciences de la Société', abbr: 'SdS', logo: Economy, color: '#fcb000' }, // TODO: logo dédié manquant
-  GSEM: { name: "Faculté d'Économie et de Management", abbr: 'GSEM', logo: Economy, color: '#425878' },
-  LAW: { name: 'Faculté de Droit', abbr: 'Droit', logo: Law, color: '#ba0c2f' },
-  THEOLOGY: { name: 'Faculté Autonome de Théologie Protestante', abbr: 'Théologie', logo: Theology, color: '#490674' },
-  PSYCHOLOGY: { name: "Faculté de Psychologie et des Sciences de l'Éducation", abbr: 'Psychologie', logo: Psychology, color: '#00b1ae' },
-  FTI: { name: "Faculté de Traduction et d'Interprétation", abbr: 'FTI', logo: Translation, color: '#fe5900' },
+  SCIENCES: { name: 'Faculté des Sciences', abbr: 'Sciences', logo: SciencesLogo, color: '#318063' },
+  MEDICINE: { name: 'Faculté de Médecine', abbr: 'Médecine', logo: MedicineLogo, color: '#9a0050' },
+  LETTERS: { name: 'Faculté des Lettres', abbr: 'Lettres', logo: LettersLogo, color: '#046fcb' },
+  SOCIAL_SCIENCES: { name: 'Faculté des Sciences de la Société', abbr: 'SdS', logo: EconomyLogo, color: '#fcb000' },
+  GSEM: { name: "Faculté d'Économie et de Management", abbr: 'GSEM', logo: SocialSciencesLogo, color: '#425878' },
+  LAW: { name: 'Faculté de Droit', abbr: 'Droit', logo: LawLogo, color: '#ba0c2f' },
+  THEOLOGY: { name: 'Faculté Autonome de Théologie Protestante', abbr: 'Théologie', logo: TheologyLogo, color: '#490674' },
+  PSYCHOLOGY: { name: "Faculté de Psychologie et des Sciences de l'Éducation", abbr: 'Psychologie', logo: PsychologyLogo, color: '#00b1ae' },
+  FTI: { name: "Faculté de Traduction et d'Interprétation", abbr: 'FTI', logo: TranslationLogo, color: '#fe5900' },
 } as const
 
 export const Faculty = {
